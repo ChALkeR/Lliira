d6b8da2fe24bf4b88f810a691aa661b61cf2d3aa
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-03-15 11:21:11 +0300

    meeting-picker: don't print duplicates when bruteforcing

--- meeting-picker.js
+++ meeting-picker.js
@@ -1,8 +1,12 @@
 const { printShort } = require('./lib/utils.js');
 const { bruteforce } = require('./lib/main.js');
 
+const had = new Set();
 for (let size = 1; size <= 3; size++) {
   for (const info of bruteforce(size)) {
+    const key = info.times.join(',');
+    if (had.has(key)) continue;
+    had.add(key);
     printShort(info);
   }
 }
