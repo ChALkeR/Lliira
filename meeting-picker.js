27a22af7a4a7a06cbbcc8e33fbd60a4ea9d767bc
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-03-15 11:36:40 +0300

    main: add a bruteforce(maxSize) wrapper

--- meeting-picker.js
+++ meeting-picker.js
@@ -1,12 +1,6 @@
 const { printShort } = require('./lib/utils.js');
 const { bruteforce } = require('./lib/main.js');
 
-const had = new Set();
-for (let size = 1; size <= 3; size++) {
-  for (const info of bruteforce(size)) {
-    const key = info.times.join(',');
-    if (had.has(key)) continue;
-    had.add(key);
-    printShort(info);
-  }
+for (const info of bruteforce(3)) {
+  printShort(info);
 }
